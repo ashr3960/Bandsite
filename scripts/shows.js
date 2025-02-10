f368fbb1d2b9a1c54d9e14da540bcea1407c56db
Rehma Ashraf <12r.ashraf@gmail.com>
2025-02-09 23:49:42 -0500

    made some changes to the stlying of the shows page added a couple of structures within the shows.js to ease in stlying for mobile modified

--- scripts/shows.js
+++ scripts/shows.js
@@ -19,30 +19,48 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         shows.forEach(show => {
             let showItem = document.createElement("li");
-            showItem.classList.add("shows__list");
+            showItem.classList.add("shows__mobile");
             tableContainer.appendChild(showItem);
-
+            
             // Date
-            let date = document.createElement("p");
-            date.classList.add("shows__list--medium");
-            date.innerText = show.date;
-            showItem.appendChild(date);
-
+            let dateWrapper  = document.createElement("div");
+            let labelDate = document.createElement("label");
+            labelDate.classList.add("shows__mobile--label", "shows__mobile--hidden");
+            labelDate.innerText = "DATE";
+            let dateContent = document.createElement("p");
+            dateContent.classList.add("shows__mobile--medium");
+            dateContent.innerText = new Date(Number(show.date)).toDateString();
+            dateWrapper .appendChild(labelDate);
+            dateWrapper .appendChild(dateContent);
+            showItem.appendChild(dateWrapper );
+  
             // Venue
-            let venue = document.createElement("p");
-            venue.classList.add("shows__list--medium");
-            venue.innerText = show.venue;
-            showItem.appendChild(venue);
-
+            let venueWrapper = document.createElement("div");
+            let labelVenue = document.createElement("label");
+            labelVenue.classList.add("shows__mobile--label", "shows__mobile--hidden");
+            labelVenue.innerText = "VENUE";
+            let venueContent = document.createElement("p");
+            venueContent.classList.add("shows__mobile--medium");
+            venueContent.innerText = show.venue;
+            venueWrapper.appendChild(labelVenue);
+            venueWrapper.appendChild(venueContent);
+            showItem.appendChild(venueWrapper);
+  
             // Location
-            let location = document.createElement("p");
-            location.classList.add("shows__list--medium");
-            location.innerText = show.location;
-            showItem.appendChild(location);
-
+            let locationWrapper = document.createElement("div");
+            let labelLocation = document.createElement("label");
+            labelLocation.classList.add("shows__mobile--label", "shows__mobile--hidden");
+            labelLocation.innerText = "LOCATION";
+            let locationContent = document.createElement("p");
+            locationContent.classList.add("shows__mobile--medium");
+            locationContent.innerText = show.location;
+            locationWrapper.appendChild(labelLocation);
+            locationWrapper.appendChild(locationContent);
+            showItem.appendChild(locationWrapper);
+  
             // Button
             let button = document.createElement("button");
-            button.classList.add("shows__list--button");
+            button.classList.add("shows__mobile--button");
             button.innerText = "BUY TICKETS";
             showItem.appendChild(button);
         });
